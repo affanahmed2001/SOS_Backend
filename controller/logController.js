3a132ea9a2008645c040fdb63cf8d661cab7af21
itteammobilogi <116087266+affanahmed2001@users.noreply.github.com>
2025-07-22 15:11:09 +0530

    Changes

--- controller/logController.js
+++ controller/logController.js
@@ -330,7 +330,7 @@ const login = async (req, res) => {
     );
 
     res.cookie('token', token, {
-      httpOnly: true,
+      // httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
       secure: false,
       sameSite: 'Strict',
