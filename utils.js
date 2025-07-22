0e98cdcc36d1c31abc33f4cf91a5ac0662308ccf
itteammobilogi <116087266+affanahmed2001@users.noreply.github.com>
2025-07-22 15:10:24 +0530

    Debugging Tokens

--- utils.js
+++ utils.js
@@ -3,7 +3,12 @@ const secretKey = process.env.JWT_TOKEN;
 
 
 const authenticationToken = (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1];
+    // const token = req.headers['authorization']?.split(' ')[1];
+    const token = req.cookies.token;
+
+    console.log("TOKEN: ", token)
+    console.log("Cookies: ", req.cookies)
+    
     
     if (!token) {
         return res.status(403).json({
